936f68798206f47c3106f9588a13ec3a868cc207
matde-je <matidlecosta719@gmail.com>
2024-08-01 18:22:59 +0100

    initial

---
+++ srcs/mariadb/config.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+service mariadb start
+
+sleep 5
+
+mariadb < ./setup.sql
+
+mysqladmin -u root -p$SQL_ROOT_PASS shutdown
+
+mysqld_safe --port=3306 --bind-address=0.0.0.0 --datadir='/var/lib/mysql'
+#default data directory for MariaDB installations
+#continuous operation 
+#restart automatically if error
